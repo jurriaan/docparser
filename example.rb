87e18035b3bfce5359a0d6ba232cc1b3f69c3e8b
Jurriaan Pruis <email@jurriaanpruis.nl>
2013-04-22 19:47:51 +0200

    Fixed example

--- example.rb
+++ example.rb
@@ -1,19 +1,19 @@
 #
-
-# An example of parsing a popular dutch website..
+# An example of parsing hackaday.com
 # (C) 2013 Jurriaan Pruis
 #
 
 require 'docparser'
 include DocParser
-output = HTMLOutput.new filename: 'hackaday.html'
+output = HTMLOutput.new(filename: 'hackaday.html')
 output.header = 'Title', 'Author', 'Publication date', 'URL', 'Summary'
-parser = Parser.new(files: (1..20).map {|i| "http://hackaday.com/page/#{i}/"}, parallel: false, output: output)
+urls = (1..20).map { |i| "http://hackaday.com/page/#{i}/" }
+parser = Parser.new(files: urls, parallel: false, output: output)
 parser.parse! do
   css('#content .post') do |post|
     title_el = post.search('.entry-title a').first
     title = title_el.content
-    author =post.search('.post-info .author .fn a').first.content
+    author = post.search('.post-info .author .fn a').first.content
     published_time = post.search('.post-info .date.published').first.content
     url = title_el.attributes['href']
     summary = post.search('.entry-content').first.content.strip
