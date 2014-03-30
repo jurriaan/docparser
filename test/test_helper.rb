6667fb09160b5bf820984f6d6fd8be19f5bbbc64
Jurriaan Pruis <email@jurriaanpruis.nl>
2014-03-30 21:39:06 +0200

    Wear Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
 require 'coveralls'
+Coveralls.wear!
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
